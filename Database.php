de21cb7481e2a7b8698e882ab450b35e4a90a128
“antoanparashkevov” <tonkata1505@gmail.com>
2023-11-27 17:41:16 +0200

    Use an http_build_query built-in method to build the dsn connection string

--- Database.php
+++ Database.php
@@ -8,7 +8,14 @@ class Database
     //this is called automatically by PHP when an instance is created
     public function __construct()
     {
-        $dsn = 'mysql:host=localhost;post=3306;dbname=myblog;charset=utf8mb4';
+        $config = [
+            'host' => 'localhost',
+            'port' => '3306',
+            'dbname' => 'myblog',
+            'charset' => 'utf8mb4'
+        ];
+        
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
         $this->connection = new PDO($dsn, 'username', 'password', [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
