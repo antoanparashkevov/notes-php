e21c7b2ba6ce25dbf09ed6cec5e444c93bed7cc1
“antoanparashkevov” <tonkata1505@gmail.com>
2023-11-27 17:33:48 +0200

    Outsource the route handler logic into a separate file

--- index.php
+++ index.php
@@ -1,35 +1,8 @@
 <?php
 
 require 'functions.php';
+require 'routes.php';
 require 'Database.php';
-
-//dd($_SERVER);
-
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-//dd(parse_url($uri));
-
-$routes = array(
-    '/' => 'controllers/index.php',
-    '/hero' => 'controllers/hero.php'
-);
-
-function abort($status_code = 404) {
-    http_response_code($status_code);
-    require "controllers/$status_code.php";
-
-    die();
-}
-
-function routeToController($uri, $routes) {
-    if(array_key_exists($uri, $routes)) {
-        require $routes[$uri];
-    } else {
-        abort();
-    }
-}
-
-//routeToController($uri, $routes);
 
 //connect to MySQL database;
 
@@ -37,4 +10,4 @@ $db = new Database();
 
 $posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
-dd($posts);
+//dd($posts);
---
+++ routes.php
@@ -0,0 +1,28 @@
+<?php
+
+//dd($_SERVER);
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+
+$routes = array(
+    '/' => 'controllers/index.php',
+    '/hero' => 'controllers/hero.php'
+);
+
+function abort($status_code = 404) {
+    http_response_code($status_code);
+    require "controllers/$status_code.php";
+
+    die();
+}
+
+function routeToController($uri, $routes) {
+    if(array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort();
+    }
+}
+
+routeToController($uri, $routes);
