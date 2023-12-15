c8be9cbafb4abec7f371db7955a50165b17beed9
“antoanparashkevov” <tonkata1505@gmail.com>
2023-12-15 18:10:37 +0200

    style the h1 tag

--- views/partials/banner.php
+++ views/partials/banner.php
@@ -1,3 +1,3 @@
 <div class="mx-auto max-w-7xl py-6 -x-4 sm:px-6 lg:px-8">
-    <h1>Welcome, <?= $_SESSION && $_SESSION['name'] ? $_SESSION['name'] : 'Guest' ?></h1>
+    <h1 class="font-bold text-xl text-gray-900">Welcome, <?= $_SESSION && $_SESSION['name'] ? $_SESSION['name'] : 'Guest' ?></h1>
 </div>
