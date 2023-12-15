5eea57b7350812af91e13e48611ed4f35676c793
“antoanparashkevov” <tonkata1505@gmail.com>
2023-12-15 20:17:18 +0200

    hash the password

--- Core/Router.php
+++ Core/Router.php
@@ -46,12 +46,7 @@ class Router
         foreach ($this->routes as $route) {
             if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
                 //apply the middleware
-
-                if($route['middleware']) {
-                   $middleware = Middleware::MAP[$route['middleware']];
-
-                   (new $middleware)->handle();
-                }
+                Middleware::resolve($route['middleware']);
 
                 return require base_path($route['controller']);
             }
--- controllers/auth/store.php
+++ controllers/auth/store.php
@@ -40,7 +40,7 @@ if( $user ) {
 } else {
     $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
         'email' => $email,
-        'password' => $password
+        'password' => password_hash($password, PASSWORD_BCRYPT)
     ]);
 
     $_SESSION['has_logged_in'] = true;
