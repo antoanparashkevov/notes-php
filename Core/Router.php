b3df059fa1ee920f8b3da69a2ec8724c16feec5c
“antoanparashkevov” <tonkata1505@gmail.com>
2023-12-15 19:54:51 +0200

    Apply two middlewares: one for guest and one when authenticated

--- Core/Router.php
+++ Core/Router.php
@@ -8,33 +8,58 @@ class Router
 {
     protected $routes = [];
 
-    protected function add($method, $uri, $controller ): void {
-        $this->routes[] = compact('method', 'uri', 'controller');
+    protected function add($method, $uri, $controller, $middleware = null ) {
+        $this->routes[] = compact('method', 'uri', 'controller', 'middleware');
+
+        return $this;
     }
 
-    public function get($uri, $controller): void
+    public function get($uri, $controller)
     {
 
-        $this->add('GET', $uri, $controller);
+        return $this->add('GET', $uri, $controller);
     }
 
-    public function post($uri, $controller): void
+    public function post($uri, $controller)
     {
-        $this->add('POST', $uri, $controller);
+        return $this->add('POST', $uri, $controller);
     }
 
-    public function delete($uri, $controller): void
+    public function delete($uri, $controller)
     {
-        $this->add('DELETE', $uri, $controller);
+        return $this->add('DELETE', $uri, $controller);
+    }
+
+    public function only($key) {
+
+        $this->routes[array_key_last($this->routes)]['middleware'] = $key;
+
+        return $this;
     }
 
     public function route($uri, $method)
     {
-//        dd($this->routes);
-//        dd($this->routes[0]);
+        //dd($this->routes);
+        //dd($this->routes[0]);
 
         foreach ($this->routes as $route) {
             if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+                //apply the middleware
+
+                if( $route['middleware'] === 'guest' ) {
+
+                    if( isset($_SESSION['user']) ) {
+                        header('location: /');
+                        die();
+                    }
+                } else if( $route['middleware'] === 'auth' ) {
+
+                    if( !isset($_SESSION['user']) ) {
+                        header('location: /');
+                        die();
+                    }
+                }
+
                 return require base_path($route['controller']);
             }
         }
--- routes.php
+++ routes.php
@@ -2,13 +2,12 @@
 
 $router->get('/', 'controllers/index.php');
 
-$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
+$router->get('/note', 'controllers/notes/show.php')->only('auth');
+$router->delete('/note', 'controllers/notes/destroy.php')->only('auth');
 
-$router->get('/note', 'controllers/notes/show.php');
-$router->delete('/note', 'controllers/notes/destroy.php');
+$router->get('/notes/create', 'controllers/notes/create.php')->only('auth');
+$router->post('/notes', 'controllers/notes/store.php')->only('auth');
 
-$router->get('/notes/create', 'controllers/notes/create.php');
-$router->post('/notes', 'controllers/notes/store.php');
-
-$router->get('/auth', 'controllers/auth/create.php');
-$router->post('/auth', 'controllers/auth/store.php');
+$router->get('/auth', 'controllers/auth/create.php')->only('guest');
+$router->post('/auth', 'controllers/auth/store.php')->only('guest');
