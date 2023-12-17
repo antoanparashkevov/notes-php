bd837c6ef0db160d24936ed89830e4447a69ae5b
“antoanparashkevov” <tonkata1505@gmail.com>
2023-12-17 11:17:53 +0200

    Modify the header and style it

--- views/partials/banner.php
+++ views/partials/banner.php
@@ -1,6 +1,9 @@
-<div class="mx-auto max-w-7xl py-6 -x-4 sm:px-6 lg:px-8">
-    <h1 class="font-bold text-xl text-gray-900">
+<div class="mx-auto max-w-7xl py-6 -x-4 sm:px-3 px-8">
+    <h1 class="font-bold text-sm text-gray-900">
         Welcome,
-        <?= isset($_SESSION['has_logged_in']) && isset($_SESSION['user']) ? $_SESSION['user']['email'] : 'Guest' ?>
+        <?= isset($_SESSION['has_logged_in']) && isset($_SESSION['user']) ?
+            $_SESSION['user']['email'] :
+            'Guest'
+        ?>
     </h1>
 </div>
--- views/partials/header.php
+++ views/partials/header.php
@@ -1,13 +1,15 @@
-<header class="absolute inset-x-0 top-0 z-50">
-    <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <div class="flex lg:flex-1">
+<header class="w-full z-50">
+    <nav class="flex items-center justify-between p-6 sm:px-8" aria-label="Global">
+
+        <div class="flex sm:flex-1">
             <a href="/" class="-m-1.5 p-1.5">
                 <span class="sr-only">My Notes</span>
-                <img class="h-8 w-auto" src="/images/notes-logo.svg" alt="Notes Logo">
+                <img class="h-8 w-auto" src="images/notes-logo.svg" alt="Notes Logo">
             </a>
         </div>
-        <div class="flex lg:hidden">
-            <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+
+        <div class="flex sm:hidden">
+            <button onclick="expandCollapseMenu()" type="button" class="-m-2.5 inline-flex items-center justify-center rounded-sm p-2.5 text-gray-700">
                 <span class="sr-only">Open main menu</span>
                 <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                      aria-hidden="true">
@@ -16,71 +18,96 @@
                 </svg>
             </button>
         </div>
-        <div class="hidden lg:flex lg:gap-x-12">
-            <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Home</a>
+
+        <div class="hidden sm:flex sm:gap-x-12">
+
+            <a href="/" class="text-sm font-semibold text-gray-900 hover:underline">Home</a>
+
             <?php if($_SESSION['has_logged_in'] ?? false) : ?>
-                <a href="/notes" class="text-sm font-semibold leading-6 text-gray-900">Notes</a>
+
+                <a href="/notes" class="text-sm font-semibold text-gray-900 hover:underline">Notes</a>
             <?php endif; ?>
         </div>
-        <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+
+        <div class="hidden sm:flex sm:flex-1 sm:justify-end">
+
             <?php if($_SESSION['has_logged_in'] ?? false) : ?>
-               <div>
-                    <form action="/session" method="post">
-                        <input type="hidden" name="_method" value="DELETE">
-                        <button class="text-black">Logout</button>
-                    </form>
-                </div>
+
+                <form action="/session" method="post">
+                    <input type="hidden" name="_method" value="DELETE">
+                    <button class="text-sm font-semibold text-gray-900 hover:underline">Logout</button>
+                </form>
             <?php else : ?>
-                <a href="/register" class="text-sm font-bold leading-6 text-gray-900 mr-4">
-                    Register
-                </a>
-                <a href="/login" class="text-sm font-bold leading-6 text-gray-900">
-                    Login
-                </a>
+
+                <div class="flex justify-center items-center gap-x-6">
+                    <a href="/register" class="text-sm font-semibold text-gray-900 hover:underline">
+                        Register
+                    </a>
+                    <a href="/login" class="text-sm font-semibold text-gray-900 hover:underline">
+                        Login
+                    </a>
+                </div>
             <?php endif; ?>
         </div>
     </nav>
-    <?= view('partials/banner.php') ?>
+
     <!-- Mobile menu, show/hide based on menu open state. -->
-    <div class="lg:hidden" role="dialog" aria-modal="true">
-        <!-- Background backdrop, show/hide based on slide-over state. -->
-        <div class="fixed inset-0 z-50"></div>
-        <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+    <div class="hidden sm:hidden" role="dialog" aria-modal="true" id="menu">
+
+        <div class="
+                fixed top-0 right-0 z-50
+                w-full h-full
+                overflow-y-auto bg-white
+                px-6 py-6
+                sm:ring-1 sm:ring-gray-900/10
+            "
+        >
+
             <div class="flex items-center justify-between">
-                <a href="/" class="-m-1.5 p-1.5">
+
+                <a href="/" class="p-1.5">
                     <span class="sr-only">My notes</span>
                     <img class="h-8 w-auto" src="/images/notes-logo.svg" alt="Notes Logo">
                 </a>
-                <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+
+                <button onclick="expandCollapseMenu()" type="button" class="rounded-md p-2.5 text-gray-700">
                     <span class="sr-only">Close menu</span>
                     <img class="h-8 w-auto" src="/images/close-button.svg" alt="Close Button">
                 </button>
             </div>
-            <div class="mt-6 flow-root">
-                <div class="-my-6 divide-y divide-gray-500/10">
-                    <div class="space-y-2 py-6">
-                        <a href="/" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+
+            <div class="mt-4 flow-root">
+
+                <div class="divide-y divide-gray-500/10">
+
+                    <div class="py-6">
+
+                        <a href="/" class="block rounded-lg px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50">
                             Home
                         </a>
+
                         <?php if($_SESSION['has_logged_in'] ?? false) : ?>
-                        <a href="/notes" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                            Notes
-                        </a>
+
+                            <a href="/notes" class="block rounded-lg px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50">
+                                Notes
+                            </a>
                         <?php endif; ?>
                     </div>
+
                     <div class="py-6">
+
                         <?php if($_SESSION['has_logged_in'] ?? false) : ?>
-                            <div>
-                                <form action="/session" method="post">
-                                    <input type="hidden" name="_method" value="DELETE">
-                                    <button class="text-black font-bold">Logout</button>
-                                </form>
-                            </div>
+
+                            <form action="/session" method="post">
+                                <input type="hidden" name="_method" value="DELETE">
+                                <button class="w-full flex px-3 py-2.5 text-base font-semibold text-gray-900 hover:bg-gray-50">Logout</button>
+                            </form>
                         <?php else : ?>
-                            <a href="/register" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+
+                            <a href="/register" class="block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                                 Register
                             </a>
-                            <a href="/login" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                            <a href="/login" class="block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                                 Login
                             </a>
                         <?php endif; ?>
@@ -90,3 +117,23 @@
         </div>
     </div>
 </header>
+
+<script>
+    let menu = document.getElementById('menu');
+
+    const expandCollapseMenu = () => {
+        console.log('clicked')
+
+        if( menu && menu.classList ) {
+
+            if( menu.classList.contains('hidden')) {
+                menu.classList.remove('hidden')
+            } else {
+                menu.classList.add('hidden')
+            }
+            // console.dir(menu)
+            // console.log(menu.classList)
+            // console.log(menu.classList.contains('hidden'))
+        }
+    }
+</script>
